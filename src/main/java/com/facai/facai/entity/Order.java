986e7cb4b719e3b12e3837607488a08b9394a769
zhangbo <854008949@qq.com>
2019-12-04 13:58:16 +0800

    20191204

--- src/main/java/com/facai/facai/entity/Order.java
+++ src/main/java/com/facai/facai/entity/Order.java
@@ -27,9 +27,13 @@ public class Order {
 
     private String oAddress;
 
+    private Date oDeliverytime;
+
+    private Date oConfirmtime;
+
     private List<OrderDetail> orderDetail;
 
-    public Order(Integer oId, String oSerialnum, Integer oPaymethod, Date oCreatetime, Integer oState, Integer oType, String oTransactionnum, BigDecimal oTotalamount, Integer oUserid, String oRemarks, String oAddress) {
+    public Order(Integer oId, String oSerialnum, Integer oPaymethod, Date oCreatetime, Integer oState, Integer oType, String oTransactionnum, BigDecimal oTotalamount, Integer oUserid, String oRemarks, String oAddress, Date oDeliverytime, Date oConfirmtime) {
         this.oId = oId;
         this.oSerialnum = oSerialnum;
         this.oPaymethod = oPaymethod;
@@ -41,6 +45,8 @@ public class Order {
         this.oUserid = oUserid;
         this.oRemarks = oRemarks;
         this.oAddress = oAddress;
+        this.oDeliverytime = oDeliverytime;
+        this.oConfirmtime = oConfirmtime;
     }
 
     public Order() {
@@ -135,11 +141,27 @@ public class Order {
         this.oAddress = oAddress == null ? null : oAddress.trim();
     }
 
+    public Date getoDeliverytime() {
+        return oDeliverytime;
+    }
+
+    public void setoDeliverytime(Date oDeliverytime) {
+        this.oDeliverytime = oDeliverytime;
+    }
+
+    public Date getoConfirmtime() {
+        return oConfirmtime;
+    }
+
+    public void setoConfirmtime(Date oConfirmtime) {
+        this.oConfirmtime = oConfirmtime;
+    }
+
     public List<OrderDetail> getOrderDetail() {
         return orderDetail;
     }
 
     public void setOrderDetail(List<OrderDetail> orderDetail) {
-        this.orderDetail = orderDetail;
+        this.orderDetail = orderDetail == null ? null : orderDetail;
     }
 }
